b3bd49826dcc08af32e6e40bce0293cc7e083ef8
sravanthi57 <dsravanthi2302@gmail.com>
2022-06-18 03:25:22 +0000

    super league

---
+++ index.js
@@ -0,0 +1,19 @@
+const element = (
+  <div className="cont">
+    <h1 className="heading">Super Over League</h1>
+    <div className="team-container">
+      <img
+        className="image-logo"
+        src="https://assets.ccbp.in/frontend/react-js/rcb-img.png"
+      />
+      <img
+        className="image-logo"
+        src="https://assets.ccbp.in/frontend/react-js/csk-img.png"
+      />
+    </div>
+  </div>
+
+  //   Write your code here.
+);
+
+ReactDOM.render(element, document.getElementById("root"));
